9e95f68facc228391b294eda7fa9f918e083efde
Paul Webster <pwebster@ca.ibm.com>
2017-02-15 15:23:17 -0500

    Remove fake backslashes

--- get-service-instances.sh
+++ get-service-instances.sh
@@ -6,6 +6,6 @@
 # Use, duplication or disclosure restricted by GSA ADP Schedule
 # Contract with IBM Corp.
 
-curl -s -H \"Authorization: ${TOOLCHAIN_TOKEN}\" \
+curl -s -H "Authorization: ${TOOLCHAIN_TOKEN}" \
 -H 'Content-Type: application/json' \
 https://devops-api.stage1.ng.bluemix.net/v1/toolchains/${PIPELINE_TOOLCHAIN_ID}/services >"$SERVICE_INSTANCE_FILE"
