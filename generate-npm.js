4a2626a14045ce82a3d2d46ec4da54fd00349aec
Paul Webster <pwebster@ca.ibm.com>
2017-02-17 18:11:09 -0500

    Switch to the new schema.

--- generate-npm.js
+++ generate-npm.js
@@ -6,33 +6,31 @@
  * Use, duplication or disclosure restricted by GSA ADP Schedule
  * Contract with IBM Corp.
  */
-
+/*eslint-env node */
 var fs = require('fs'); 
-var services = JSON.parse(fs.readFileSync('/tmp/tc_services.json')).services; 
+var services = JSON.parse(fs.readFileSync(process.env.SERVICE_INSTANCE_FILE)).services;
 
-var npm_mirror = findServiceInstance(services, 'npm-mirror');
-if (npm_mirror) {
-    console.log('export NPM_MIRROR_URL="' + npm_mirror.dashboard_url + '"');
-}
-
-var npm_si = findServiceInstance(services, '-npm');
-if (npm_si) {
-    var name = npm_si.parameters.name,
-        url = npm_si.parameters.dashboard_url,
-        user_id = npm_si.parameters.user_id,
-        token = npm_si.parameters.token,
-        id = name.substring(0, name.length-4);
+var npm_service = findServiceInstance(services, process.env.SERVICE_INSTANCE_TYPE, process.env.SERVICE_INSTANCE);
+if (npm_service) {
+    var id = npm_service.parameters.name,
+        url = npm_service.parameters.dashboard_url,
+        mirror_url = npm_service.parameters.mirror_url,
+        snapshot_url = npm_service.parameters.snapshot_url,
+        user_id = npm_service.parameters.user_id,
+        token = npm_service.parameters.token;
     console.log('export NPM_NAME="' + id + '"');
     console.log('export NPM_RELEASE_URL="' + url + '"');
     console.log('export NPM_USER_ID="' + user_id + '"');
     console.log('export NPM_TOKEN="' + token + '"');
+    console.log('export NPM_MIRROR_URL="' + mirror_url + '"');
+    console.log('export NPM_SNAPSHOT_URL="' + snapshot_url + '"');
 }
 
-function findServiceInstance(services, suffix) {
+function findServiceInstance(services, type, name) {
     var nexus = services.filter(function (v) {
-        return v.service_id === 'nexus'
-            && v.parameters && v.parameters.name
-            && v.parameters.name.endsWith(suffix);
+        return v.service_id === type
+            && (name  === '(default)'
+                || v.parameters && v.parameters.name === name);
     });
     if (nexus.length>0) {
         return nexus[0];
